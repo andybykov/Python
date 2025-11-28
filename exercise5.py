02be2dae7d7297ebfec23404b5c0ad40be01aba7
AndyBykov <portperm@yandex.ru>
2025-11-29 00:41:04 +0300

    Exercise 5. Add visualisation

--- exercise5.py
+++ exercise5.py
@@ -4,19 +4,34 @@ from PIL import Image, ImageDraw
 
 class GameOfLife:
     # Конструктор класса
-    def __init__(self, input_filename, output_filename,  generations=-1):
+    def __init__(self, input_filename, output_filename_txt, output_filename_png,  generations=-1):
        self.dirfile = "/home/andybyokv/Python_PJCS/PythonExercises/" 
        self.intput_filename = self.dirfile + input_filename
-       if output_filename == '':
-           self.output_filename = ''
+
+       if output_filename_txt == '':
+           self.output_filename_txt = ''
        else:
-           self.output_filename = self.dirfile + output_filename          
+           self.output_filename_txt = self.dirfile + output_filename_txt 
+
+       if output_filename_png== '':
+           self.output_filename_png= ''
+       else:
+           self.output_filename_png = self.dirfile + output_filename_png     
+
        self.grid = self.load_from_file()       
        self.rows = len(self.grid) # вычисляем длину строки
        self.cols = len(self.grid[0]) # вычисляем количество столбцов по 0 строке
        self.generations = generations  # количество генераций  
-       self.iterations = 0 # количество итераций    
-       self.gen_finish = False # флаг окончания     
+       self.iterations = 1 # количество итераций    
+       self.gen_finish = False # флаг окончания 
+
+        # Параметры визуализации
+       self.base_color = (0, 255, 0)   # зелёный
+       self.cell_size = 20
+       self.border_width = 2
+        
+       # Для отслеживания возраста клеток
+       self.age_grid = [[0 for _ in range(self.cols)] for _ in range(self.rows)]
 
     def neighbors_counter(self, row, col):
         """ Счетчик соседей """
@@ -48,34 +63,41 @@ class GameOfLife:
         """ Вычисляет следующее поколение """    
         new_grid = [[0 for _ in range(self.cols)] for _ in range(self.rows)]
 
+        new_age_grid = [[0 for _ in range(self.cols)] for _ in range(self.rows)]
+
         for row in range(self.rows):           # Y
             for col in range(self.cols):       # X
                 neighbors = self.neighbors_counter(row, col)
-
                 current_cell = self.grid[row][col]  # запоминаем текущее состояние ИСХОДНОГО поля
+                current_age = self.age_grid[row][col]
         
                 if current_cell == 1:  # ЖИВАЯ клетка
                     if neighbors == 2 or neighbors == 3:
                         new_grid[row][col] = 1  # остается живой
+                        new_age_grid[row][col] = current_age + 1  # Увеличиваем возраст
                     else:
                         new_grid[row][col] = 0  # умирает
+                        new_age_grid[row][col] = 0  # Клетка умирает, возраст обнуляется
 
                 if current_cell == 0:  # ПУСТАЯ клетка 
                     if neighbors == 3:
                         new_grid[row][col] = 1  # рождается
+                        new_age_grid[row][col] = 1  # Новая клетка, возраст = 1
                     else:
-                        new_grid[row][col] = 0  # остается пустой     
+                        new_grid[row][col] = 0  # остается пустой   
+                        new_age_grid[row][col] = 0  
 
         if self.grid == new_grid:
             self.gen_finish = True
         
         self.grid = new_grid
+        self.age_grid = new_age_grid  # обновляем возраст
         return new_grid
      
     def write_to_file(self, generation):
         """Пишет в файл"""
-        if self.output_filename != '':
-            path = self.output_filename
+        if self.output_filename_txt != '':
+            path = self.output_filename_txt
             lines = []
             lines.append(f"Generation {generation}")
             for row in self.grid:
@@ -86,6 +108,76 @@ class GameOfLife:
             with open(path, 'a', encoding='utf-8') as f:
                 f.write(text)
         return          
+
+    def save_image(self, iteration: int) -> None:
+        """Создаёт PNG-файл с оттенками базового цвета в зависимости от возраста клеток"""
+        if self.output_filename_png == '':
+            return
+
+        # Размеры изображения+ границы
+        width = self.cols * (self.cell_size + self.border_width) + self.border_width
+        height = self.rows * (self.cell_size + self.border_width) + self.border_width
+        
+        # изображение с фоном
+        img = Image.new("RGB", (width, height), (30, 30, 30))
+        draw = ImageDraw.Draw(img)
+        
+        # вертикальные линии
+        for x in range(0, width, self.cell_size + self.border_width):
+            draw.line(
+                (x, 0, x, height),
+                fill=(50, 50, 50),
+                width=self.border_width
+            )
+        
+        # горизонтальные линии
+        for y in range(0, height, self.cell_size + self.border_width):
+            draw.line(
+                (0, y, width, y),
+                fill=(50, 50, 50),
+                width=self.border_width
+            )
+        
+        # клетки 
+        for row in range(self.rows):
+            for col in range(self.cols):
+                if self.grid[row][col] == 1:
+                    
+                    age = self.age_grid[row][col] # получаем возраст
+                    
+                    max_age = 10  # Максимальный возраст для шкалы темноты
+                    age_factor = min(age, max_age) / max_age # маппим 0.0 - 1.0
+
+                     # затемнение 
+                    """
+                        1.0 - максимальная яркость                     
+                        0.7 - коэффициент затемнения 
+                    """
+                    brightness = 1.0 - (age_factor * 0.7)  # От 1.0 до 0.2 
+
+                    # компоненты RGB цвета                   
+                    r = int(self.base_color[0] * brightness) # Умножаем каждый компонент базового цвета на яркость
+                    g = int(self.base_color[1] * brightness)
+                    b = int(self.base_color[2] * brightness)
+                    
+                    # Координаты левого верхнего угла клетки + границы
+                    x0 = col * (self.cell_size + self.border_width) + self.border_width
+                    y0 = row * (self.cell_size + self.border_width) + self.border_width
+
+                    # Координаты правого нижнего угла клетки                    
+                    x1 = x0 + self.cell_size - 1 # -1px 
+                    y1 = y0 + self.cell_size - 1 # -1px 
+                    
+                    # Рисуем саму клетку                
+                    draw.rectangle(
+                        [x0, y0, x1, y1], # координаты прямоугольника
+                        fill=(r, g, b) # цвет заливки 
+                    )
+
+        # Сохраняем изображение              
+        img.save(f"{self.output_filename_png}/IMG{iteration}.png")
+        print(f"Saved to: {self.output_filename_png}/IMG{iteration}.png")
+       
 
     def load_from_file(self):
         """Загружеает данные из файла"""
@@ -98,8 +190,8 @@ class GameOfLife:
                 #print(self.grid)
                 return self.grid
             except Exception as ex:
-                print(ex)           
-            
+                print(ex)                
+
     def display(self):
         """Распечатка на консоль"""
         os.system('clear')  # Очищаем консоль
@@ -121,32 +213,36 @@ class GameOfLife:
     def run_simulation(self):
         """Запуск симуляции """       
         def run():
-            gen_count = 1 # счетчик поколений
-            continue_gen = self.next_gen()            
+            self.save_image(self.iterations)   # PIL 
+            self.next_gen()            
             self.display()
-            self.write_to_file(gen_count)
-            gen_count += 1
+            self.write_to_file(self.iterations)
+            self.iterations +=1 
+                               
             
-            return continue_gen
-
         if self.generations > 0: # указанное количество итераций                     
             for _ in range(self.generations):
                 run()
                 if self.gen_finish == True:                    
                     break 
                 time.sleep(0.3)  # Пауза 
-            return        
+            self.gen_finish = True    
+
+            return      
+        
         elif self.generations == -1: # бесконечно
             while True:
                 run()
                 if self.gen_finish == True:                    
                     break        
-                self.iterations +=1 # ---        
-                time.sleep(0.3)  # Пауза              
+                       
+                time.sleep(0.3)  # Пауза   
+
+                          
 
 def main():
 
-    game = GameOfLife("input.txt","") 
+    game = GameOfLife("input.txt","", "img") 
     
     try:
         game.run_simulation()
